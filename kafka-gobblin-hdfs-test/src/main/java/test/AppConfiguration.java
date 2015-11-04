27029489b5e7fb6238a2d49ec54fbed64ea640c2
mark1900 <mark1900@users.noreply.github.com>
2015-11-04 18:13:16 -0500

    Update kafka-gobblin-hdfs-test AppConfiguration to fetch all properties from kafka-gobblin-hdfs-test.pull. * java -cp kafka-gobblin-hdfs-test-0.0.0.jar test.AppConfiguration * Works in Eclipse as well.

--- kafka-gobblin-hdfs-test/src/main/java/test/AppConfiguration.java
+++ kafka-gobblin-hdfs-test/src/main/java/test/AppConfiguration.java
@@ -10,30 +10,52 @@ import java.util.Properties;
 public class AppConfiguration
 {
 
-    public static final String KAFKA_SERVERS_CONFIG = System.getProperty(
-        "kafka-gobblin-hdfs-test.kafka.servers.config", "kafka-hostname:6667" );
+    public static final String KAFKA_SERVERS_CONFIG;
 
-    public static final String KAFKA_SERVERS_TOPIC = System.getProperty(
-        "kafka-gobblin-hdfs-test.kafka.servers.config.topic.test", "kafka-gobblin-hdfs-test" );
+    public static final String KAFKA_SERVERS_TOPIC;
 
 
     private static Properties properties = new Properties();
 
     static {
-        try (InputStream in =
-                AppConfiguration.class.getResourceAsStream("/kafka-gobblin-hdfs-test.pull")) //$NON-NLS-1$
+        try ( InputStream in = AppConfiguration.class.getResourceAsStream( "/kafka-gobblin-hdfs-test.pull" ) )
         {
             properties.load(in);
         }
-        catch ( IOException e )
+        catch (IOException e)
         {
             throw new RuntimeException(e);
         }
 
+        KAFKA_SERVERS_CONFIG = System.getProperty(
+            "kafka-gobblin-hdfs-test.kafka.servers.config",
+            getGobblinProperty( "kafka.brokers", "localhost:6667" ) );
+
+        KAFKA_SERVERS_TOPIC = System.getProperty(
+            "kafka-gobblin-hdfs-test.kafka.servers.config.topic.test", "kafka-gobblin-hdfs-test" );
+    }
+
+    private AppConfiguration()
+    {
+        // NOOP
     }
 
     public static String getGobblinProperty(String key)
     {
-        return properties.getProperty( key );
+        return properties.getProperty(key);
+    }
+
+    public static String getGobblinProperty(String key, String defaultValue)
+    {
+        return properties.getProperty(key, defaultValue);
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "\n\n-- System Properties --\n\n" );
+        System.getProperties().list( System.out );
+
+        System.out.println( "\n\n-- Gobblin Properties --\n\n" );
+        properties.list( System.out );
     }
 }
