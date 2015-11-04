3cac9d0632ef6e1bbe3d696e1fd258764cc71887
mark1900 <mark1900@users.noreply.github.com>
2015-11-04 17:34:51 -0500

    Update Configuration.

--- spark-hdfs-avro-test/src/main/java/test/AppConfiguration.java
+++ spark-hdfs-avro-test/src/main/java/test/AppConfiguration.java
@@ -11,10 +11,10 @@ public class AppConfiguration
     public static final boolean RUN_SPARK_LOCALLY = Boolean.parseBoolean( System.getProperty( "test.spark-hdfs-avro-test.run_spark_locally", "true" ) );
 
     // Apache Ambari Deployment
-    public static final String HADOOP_BASE_URI = System.getProperty( "test.spark-hdfs-avro-test.hadoop.uri.base", "hdfs://server1:8020" );
+    public static final String HADOOP_BASE_URI = System.getProperty( "test.spark-hdfs-avro-test.hadoop.uri.base", "hdfs://hadoop-hostname:8020" );
 
     // Standard Deployment
-//    public static final String HADOOP_BASE_URI = System.getProperty( "test.spark-hdfs-avro-test.hadoop.uri.base", "hdfs://server1:9000" );
+//    public static final String HADOOP_BASE_URI = System.getProperty( "test.spark-hdfs-avro-test.hadoop.uri.base", "hdfs://hadoop-hostname:9000" );
 
     public static final String HADOOP_INPUT_FILE_PATH = System.getProperty(
         "test.spark-hdfs-avro-test.hadoop.file.input.path", "/gobblin/work/job-output/my-test-input.avro" );
